dhacker29/android_device_oppo_find7

Add logo.bin to the build
Change-Id: I210b3a09ee770c15b51282336fb5df77d8539d49

releasetools.py
@@ -1,0 +1,21 @@
+# Copyright (C) 2012 The Android Open Source Project
+# Copyright (C) 2014 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Custom OTA commands for find7a devices"""
+
+def FullOTA_InstallEnd(info):
+    info.script.AppendExtra('ui_print("{x}...Flashing Gummy Logo Image...");')
+    info.script.AppendExtra('package_extract_file("system/logo.bin", "/dev/block/platform/msm_sdcc.1/by-name/LOGO");')
+    info.script.AppendExtra('delete("/system/logo.bin");')

cphelps76/android_device_oppo_find7-common

Conflicts:
	BoardConfig.mk
	device_find7a.mk

releasetools.py
@@ -1,0 +1,21 @@
+# Copyright (C) 2012 The Android Open Source Project
+# Copyright (C) 2014 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Custom OTA commands for find7 devices"""
+
+def FullOTA_InstallEnd(info):
+    info.script.AppendExtra('ui_print("{x}...Flashing Gummy Logo Image...");')
+    info.script.AppendExtra('package_extract_file("system/logo.bin", "/dev/block/platform/msm_sdcc.1/by-name/LOGO");')
+    info.script.AppendExtra('delete("/system/logo.bin");')